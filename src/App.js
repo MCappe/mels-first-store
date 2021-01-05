d8a9abd8991ba119ea498a0bd47add0d6f47f433
MCappe <melissacappe@gmail.com>
2021-01-04 20:49:41 -0500

    initial commit :boom:

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [userName, setUserName] = useState('');
+
+  useEffect(() => {
+    if(loggedIn) {
+      setUserName('Melissa!');
+    } else {
+      setUserName('');
+    }
+  }, [loggedIn]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to Everyday is Monday</h1>
+      
+      {/* Message for the person when they log in - figure out how to make it dynamic based on the user, not just 'Melissa' */}
+      {
+        loggedIn ? <h2>Hi, {userName}</h2> : null
+      }
+
+      <button onClick={() => setLoggedIn(!loggedIn)}>Log In</button>
     </div>
   );
 }
 
 export default App;
+
+// App called Everyday is Monday
+// "fake" e-com store where users can purchase t-shirts and other items with fun/sarcastic sayings on them - mostly "pessimistic-type" and cynical
+
+// Functionality
+// 1) Intro page welcoming users to the store - options to search by current mood, or a specific saying
+// 2) Sort items that come back by most relevant, with options to sort by price/popularity
+// 3) When items added to basket, it will pop across the right side of the screen with an ease-in-out effect
+// 4) Use firebase or localstorage to keep track of items?
---
+++ src/inventory.js
@@ -0,0 +1,184 @@
+const shirts = [
+  {
+    path: require('../assets/assassin.png'),
+    alt: 'good morning I see the assassins have failed',
+    title: 'assassin',
+    price: 12.99,
+    inventory: 5,
+    category: 'cynical',
+    shirtId: 'shirt1'
+  },
+  {
+    image: '',
+    alt: '',
+    title: '',
+    price: 12.99,
+    inventory: 5,
+    category: '',
+    shirtId: ''
+  },
+  {
+    image: '',
+    alt: '',
+    title: '',
+    price: 12.99,
+    inventory: 5,
+    category: '',
+    shirtId: ''
+  },
+  {
+    image: '',
+    alt: '',
+    title: '',
+    price: 12.99,
+    inventory: 5,
+    category: '',
+    shirtId: ''
+  },
+  {
+    image: '',
+    alt: '',
+    title: '',
+    price: 12.99,
+    inventory: 5,
+    category: '',
+    shirtId: ''
+  },
+  {
+    image: '',
+    alt: '',
+    title: '',
+    price: 12.99,
+    inventory: 5,
+    category: '',
+    shirtId: ''
+  },
+  {
+    image: '',
+    alt: '',
+    title: '',
+    price: 12.99,
+    inventory: 5,
+    category: '',
+    shirtId: ''
+  },
+  {
+    image: '',
+    alt: '',
+    title: '',
+    price: 12.99,
+    inventory: 5,
+    category: '',
+    shirtId: ''
+  },
+  {
+    image: '',
+    alt: '',
+    title: '',
+    price: 12.99,
+    inventory: 5,
+    category: '',
+    shirtId: ''
+  },
+  {
+    image: '',
+    alt: '',
+    title: '',
+    price: 12.99,
+    inventory: 5,
+    category: '',
+    shirtId: ''
+  },
+  {
+    image: '',
+    alt: '',
+    title: '',
+    price: 12.99,
+    inventory: 5,
+    category: '',
+    shirtId: ''
+  },
+  {
+    image: '',
+    alt: '',
+    title: '',
+    price: 12.99,
+    inventory: 5,
+    category: '',
+    shirtId: ''
+  },
+  {
+    image: '',
+    alt: '',
+    title: '',
+    price: 12.99,
+    inventory: 5,
+    category: '',
+    shirtId: ''
+  },
+  {
+    image: '',
+    alt: '',
+    title: '',
+    price: 12.99,
+    inventory: 5,
+    category: '',
+    shirtId: ''
+  },
+  {
+    image: '',
+    alt: '',
+    title: '',
+    price: 12.99,
+    inventory: 5,
+    category: '',
+    shirtId: ''
+  },
+  {
+    image: '',
+    alt: '',
+    title: '',
+    price: 12.99,
+    inventory: 5,
+    category: '',
+    shirtId: ''
+  },
+  {
+    image: '',
+    alt: '',
+    title: '',
+    price: 12.99,
+    inventory: 5,
+    category: '',
+    shirtId: ''
+  },
+  {
+    image: '',
+    alt: '',
+    title: '',
+    price: 12.99,
+    inventory: 5,
+    category: '',
+    shirtId: ''
+  },
+  {
+    image: '',
+    alt: '',
+    title: '',
+    price: 12.99,
+    inventory: 5,
+    category: '',
+    shirtId: ''
+  },
+  {
+    image: '',
+    alt: '',
+    title: '',
+    price: 12.99,
+    inventory: 5,
+    category: '',
+    shirtId: ''
+  }
+];
+
+export default inventory;
